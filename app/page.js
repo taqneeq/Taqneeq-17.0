e3ebd88f44f6898211ed716f3569430859c0c30b
Aanchal <aanchalshah1810@gmail.com>
2025-01-25 03:30:25 +0530

    added competitions page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "@/app/screens/hero";
 import About from "@/app/screens/about";
+import Competitions from "@/app/screens/competitions";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
       <About/>
+      <Competitions/>
     </div>
   );
 }
---
+++ app/screens/competitions/index.js
@@ -0,0 +1,37 @@
+"use client";
+import React from 'react'
+import Image from "next/image";
+import C1 from "./comp1.svg";
+export default function competition() {
+    const comp = [C1, C1, C1, C1];
+}
+const index = () => {
+  return (
+    <section
+      className="min-h-[90vh] max-h-[1440px] max-w-7xl flex flex-col justify-center items-center relative md:gap-5 mx-auto"
+    >
+        <hr className="w-1/2 border-4 absolute top-10 -left-1/4 text-brandOffWhite opacity-70"></hr>
+        <h1 className="font-BSD text-5xl text-white absolute top-5 left-1/4 ml-4">/ [ Competitions ]</h1>
+        <div
+        
+        className="w-full grid grid-cols-1 sm:grid-cols-3  gap-4 justify-items-center"
+        
+      >
+        {comp.map((comp, index) => (
+          <div
+            key={index}
+            className="relative w-full h-80"
+          >
+            <Image
+              src={comp}
+              alt={`Competition ${index + 1}`}
+              fill
+              className="object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
+
