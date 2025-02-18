6026dfc14be15f090249e738c8902cf962a36f62
Aanchal <aanchalshah1810@gmail.com>
2025-02-18 22:02:26 +0530

    events

--- app/screens/events/index.js
+++ app/screens/events/index.js
@@ -9,25 +9,24 @@ import Event5 from "./Event5.svg";
 import Event6 from "./Event6.svg";
 import Event7 from "./Event7.svg";
 import Event8 from "./Event8.svg";
-export default function competition() {
-    const events = [Event1, Event2, Event3, Event4, Event5, Event6, Event7, Event8];
+
+export default function Competition() {
+  const events = [Event1, Event2, Event3, Event4, Event5, Event6, Event7, Event8];
 
   return (
     <section
-      className="min-h-[90vh] max-h-[1440px] max-w-7xl flex flex-col justify-center items-center relative md:gap-5 mx-auto" id='events'
+      className="min-h-screen max-w-7xl flex flex-col justify-center items-center relative md:gap-5 mx-auto" 
+      id="events"
     >
-        <hr className="w-1/2 border-2 md:border-4 absolute top-10 -left-1/4 text-brandOffWhite opacity-70"></hr>
-        <h1 className="font-BSD text-4xl md:text-5xl text-white absolute top-5 left-1/4 ml-4">/ [ Events ]</h1>
-        <div
-        
-        className="w-full mt-24 md:mt-24 grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center"
-        
-      >
+      <hr className="w-1/2 border-2 md:border-4 absolute top-10 -left-1/4 text-brandOffWhite opacity-70" />
+      <h1 className="font-BSD text-4xl md:text-5xl text-white absolute top-5 left-1/4 ml-4 mb-8">
+        / [ Events ]
+      </h1>
+
+      {/* Added 'relative' to fix absolute element interference */}
+      <div className="relative w-full mt-32 md:mt-24 grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center">
         {events.map((event, index) => (
-          <div
-            key={index}
-            className="relative w-full h-96"
-          >
+          <div key={index} className="relative w-full h-96">
             <Image
               src={event}
               alt={`Event ${index + 1}`}
@@ -37,8 +36,6 @@ export default function competition() {
           </div>
         ))}
       </div>
-      
     </section>
-  )
+  );
 }
-
